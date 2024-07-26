a7c1d328d2a60f6d69676279cc3b74b089392567
vinay kumar <134502074+vinayk7324@users.noreply.github.com>
2024-07-27 03:01:08 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -40,16 +40,16 @@ const router = createBrowserRouter([
   
   
   {
-    path:"login-failed",
+    path:"/login-failed",
     element:<LoginFailed/>
 
   },
   {
-    path:'practice',
+    path:'/practice',
     element:<Practice/>
   },
   {
-    path:"twitter-home-page",
+    path:"/twitter-home-page",
     element:<TwitterUI/>,
     children:[
       {
